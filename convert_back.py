e4ad46d956fadbcdf21781bddefb0ef408a337ac
Markus <markus.stoye@cern.ch>
2016-10-05 15:00:59 +0200

    more user firendly

--- convert_back.py
+++ convert_back.py
@@ -2,7 +2,7 @@ import ROOT
 from root_numpy import array2root
 import numpy
 import numpy.lib.recfunctions
-myarray = numpy.load("testresult.npy")
+myarray = numpy.load("/afs/cern.ch/user/m/mstoye/test_result.npy")
 print myarray
 #AllSruct = myarray.view(numpy.recarray)
 #print AllSruct
@@ -10,4 +10,4 @@ print myarray
 #AllSruct.dtype.names = 'nama'
 #print AllSruct.dtype
 
-array2root(myarray,"prob.root",mode="recreate")
+array2root(myarray,"prob_M.root",mode="recreate")
--- convert_tree_numpy.py
+++ convert_tree_numpy.py
@@ -1,86 +1,73 @@
 import ROOT
 #rfile = ROOT.TFile("/afs/cern.ch/work/m/mstoye/DeepBs/CMSSW_8_0_12/src/RecoBTag/TagVarExtractor/test/JetTaggingVariables.root")
 #tree = rfile.Get("tagVars/ttree")
-rfile = ROOT.TFile("QCD_test.root")
-tree = rfile.Get("ttree")
-#tree.Show(5)
-from root_numpy import tree2array
-import numpy
-import numpy.lib.recfunctions
-Tuple = tree2array(tree)
-BranchList = Tuple.dtype.names
 
+
+def convertToTree (name, train) :
+    rfile = ROOT.TFile(name+".root")
+    print 'Converting', name+".root", " to ",name+".npy"
+    tree = rfile.Get("ttree")
+#tree.Show(5)
+    from root_numpy import tree2array
+    import numpy
+    import numpy.lib.recfunctions
+    Tuple = tree2array(tree)
+    BranchList = Tuple.dtype.names
+    
 #import random
 #print ' first ' , Tuple
-#numpy.random.shuffle(Tuple)
-#numpy.random.shuffle(Tuple)
-#numpy.random.shuffle(Tuple)
+    if train:
+        numpy.random.shuffle(Tuple)
+        numpy.random.shuffle(Tuple)
+        numpy.random.shuffle(Tuple)
+        print ' reshuffeled ', Tuple
 
-print ' reshuffeled ', Tuple
-
-#print ' the following is input to x'
-for index, Names in enumerate(BranchList):
-    print 'arrray',index, ' :' , Names
-#numpy.random.shuffle(Tuple)
-
+#    print ' the following is input to x'
+#    for index, Names in enumerate(BranchList):
+#        print 'arrray',index, ' :' , Names
 # jet cleaning
-
 #Jet_flavour_c =Tuple['Jet_flavour']
 #for i in range (Tuple.shape[0]):
 #    if abs(Jet_flavour_c[i]) == 4:
 #        Tuple = numpy.delete(Tuple,(i),axis=0)
 #Tuple = Tuple[~(Tuple['Jet_flavour']==2)]
 
-Jet_flavour =Tuple['Jet_flavour']
-    
-Y = abs(Jet_flavour)==5
-NoY =  abs(Jet_flavour)!=5
-noY = 1.*NoY
-yesy =1.*Y # converts bool to int.
+    Jet_flavour =Tuple['Jet_flavour']
+    # adds getting the truth from flavor
+    Y = abs(Jet_flavour)==5
+    NoY =  abs(Jet_flavour)!=5
+    noY = 1.*NoY# converts bool to float
+    yesy =1.*Y # converts bool to float
+    Cs = abs(Jet_flavour)==4
+    Cs_float = 1.*Cs
+    U =  abs(Jet_flavour) == 1
+    D =  abs(Jet_flavour) == 2
+    S =  abs(Jet_flavour) == 3
+    G =  abs(Jet_flavour) == 21
+    Light = [U,D,S,G]
+    Light_sum = numpy.sum(Light,axis = 0)
+    Light_sum_float = Light_sum*1
 #yesy.reshape(1,len(yesy))
-y =  numpy.column_stack((yesy,noY))
-y = y.astype('float32')
-print 'y shape ' , y.shape, ' other shapes ',  noY.shape , ' ' , yesy.shape
-print y
+    y = numpy.column_stack((yesy,noY,Cs_float ,Light_sum_float))
+    y = y.astype('float32')
+    #print 'y shape ' , y.shape, ' other shapes ',  noY.shape , ' ' , yesy.shape
+    #print y
 #x = numpy.lib.recfunctions.drop_fields(Tuple,['Jet_genpt','Jet_phi','Jet_mass','Jet_flavour','Jet_nbHadrons','Jet_JP','Jet_JBP','Jet_CSV','Jet_CSVIVF'])
-x = numpy.lib.recfunctions.drop_fields(Tuple,['Jet_genpt','Jet_phi','Jet_mass','Jet_flavour','Jet_nbHadrons','Jet_CSV'])
+    x = numpy.lib.recfunctions.drop_fields(Tuple,['Jet_flavour','Jet_nbHadrons','Jet_cbHadrons','Jet_CSV'])
 #x = numpy.lib.recfunctions.drop_fields(Tuple,['Jet_genpt','Jet_phi','Jet_mass','Jet_flavour','Jet_nbHadrons','Jet_CSV'])
+#    mynames = x.dtype.names
+#    print 'number of branches ', len(mynames) 
+#    for index , name in enumerate(mynames):
+#        print 'branch index: ' ,index, ' , branch name: ',name
 
-mynames = x.dtype.names
-print 'number of branches ', len(mynames) 
-for index , name in enumerate(mynames):
-    print 'branch index: ' ,index, ' , branch name: ',name
+    if(train):
+        numpy.save(name+"_X.npy", x)
+        numpy.save(name+"_Y.npy", y)
+    else:
+        numpy.save(name+"_X.npy", x)
+        numpy.save(name+"_Y.npy", y)
 
 
-# converts from structured array to simple array
-#X = x.view(numpy.float32).reshape(x.shape + (-1,))
-# Assuming that -99 is used if a feature is not present all features are mean subtracted and brought to standard deviation of 1 excluding the values below -90. All values smaller -90 are set to 0.
 
-
-#for i in range (0):
-#    print i;
-#    X[:,i][ numpy.isnan(X[:,i])] = 1.
-#    Cur = X[:,i]
-#    CutTHres = Cur[Cur>-90]
-    #print 'before a threshold ' , X[:,i].size, 'before a threshold ' , CutTHres.size
-#    CutTHresMean=CutTHres.mean(axis=0)
-#    CutTHresStdv=CutTHres.std(axis=0)
-    #print('mean and std ', CutTHresMean, ' ' ,CutTHresStdv)
-    #CutTHresMean = CutTHresMean.reshape((X.shape[0]), 1)
-#    X[:,i] = numpy.subtract(X[:,i],CutTHresMean)
-    #print('before X mean and std ', X[:,i].mean(axis=0) , ' ' ,X[:,i].std(axis=0))
-#    X[:,i][ X[:,i]<-90] = 0.
-#    X[:,i] = numpy.divide( X[:,i],CutTHresStdv)
-#    print('X mean and std ', X[:,i].mean(axis=0) , ' ' ,X[:,i].std(axis=0))
-
-#import matplotlib.pyplot as plt
-#plt.hist(X[0,i])
-
-numpy.save("MyExtractFileXQCD_test.npy", x)
-numpy.save("MyExtractFileYQCD_test.npy", y)
-
-#print array.x
-#print array.i
-#print tree.to_array()
-#tree = 
-
+convertToTree("QCD2C_train",train=True)
+convertToTree("QCD2C_test",train=False)
